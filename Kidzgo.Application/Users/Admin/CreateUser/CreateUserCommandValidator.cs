7335af8004e8aec47f8d8d5c3df5acb02523e96d
Nguyn <phamphuckhang24@gmail.com>
2026-01-07 22:26:23 +0700

    Fix: Update CreateUser validator - remove NotEmpty() for enum Role, add email and password validation

--- Kidzgo.Application/Users/Admin/CreateUser/CreateUserCommandValidator.cs
+++ Kidzgo.Application/Users/Admin/CreateUser/CreateUserCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Kidzgo.Domain.Users;
 
 namespace Kidzgo.Application.Users.Admin.CreateUser;
 
@@ -7,7 +8,10 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
     public CreateUserCommandValidator()
     {
         RuleFor(command => command.Name).NotNull().NotEmpty();
-        RuleFor(command => command.Email).NotNull().NotEmpty();
-        RuleFor(command => command.Role).IsInEnum().NotNull().NotEmpty();
+        RuleFor(command => command.Email).NotNull().NotEmpty().EmailAddress();
+        RuleFor(command => command.Password).NotNull().NotEmpty().MinimumLength(6);
+        RuleFor(command => command.Role)
+            .IsInEnum()
+            .WithMessage("Role must be a valid value: Admin, Staff, Teacher, Student, or Parent");
     }
 }
