7574cf395c33ba24d7219e261a189748ba86b732
Nguyn <phamphuckhang24@gmail.com>
2026-04-02 22:47:11 +0700

    update tạo class với session

--- Kidzgo.Domain/Programs/Program.cs
+++ Kidzgo.Domain/Programs/Program.cs
@@ -12,7 +12,6 @@ public class Program : Entity
     public Guid BranchId { get; set; }
     public string Name { get; set; } = null!;
     public string Code { get; set; } = null!;
-    public int TotalSessions { get; set; }
     public decimal DefaultTuitionAmount { get; set; }
     public decimal UnitPriceSession { get; set; }
     public string? Description { get; set; }
